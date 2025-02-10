60ec0256ea2f08226b46538bb7ffe377bfc1d603
DennisBozzi <dennisbozzii@gmail.com>
2025-02-10 07:42:35 -0300

    PrimeNG - PrimeFlex - PrimeIcons. Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputIcon } from 'primeng/inputicon';
+import { IconField } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, InputIcon, IconField, InputTextModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
